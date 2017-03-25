1fed44b8ab6588f43e3c02f997b7f122dce4546d
stella huayhuaca <luces.huayhuaca@gmail.com>
2017-03-25 14:40:05 -0700

    add update action for question

--- app/routes/question.js
+++ app/routes/question.js
@@ -5,6 +5,15 @@ export default Ember.Route.extend({
     return this.store.findRecord('question', params.question_id);
   },
   actions: {
+    update(question, params){
+      Object.keys(params).forEach(function(key){
+        if(params[key] !==undefined){
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('question');
+    },
     destroyQuestion(question) {
      var answer_deletions = question.get('answers').map(function(answer) {
        return answer.destroyRecord();
